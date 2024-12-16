32cae452b3ab2bda5739f4ea0fd1a34f495142f1
Yashwanth-krishna-14 <krishnaYashwanthrj@gmail.com>
2024-12-16 10:23:57 +0530

    comm

--- com/src/main/java/first/com/Devil.java
+++ com/src/main/java/first/com/Devil.java
@@ -1,10 +1,14 @@
 package first.com;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Devil {
     @Autowired  // ** automatically connects devil.java and laptop.java , since devil.java is dependent on laptop.java
+
+    @Qualifier("desktop")  // instead of using @primary in the bean , we can mention the instance(bean name)
+
     private computer comp;
 
   /*  public void dev(laptop laptop){
--- com/src/main/java/first/com/desktop.java
+++ com/src/main/java/first/com/desktop.java
@@ -1,21 +1,13 @@
 package first.com;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-    @Component
-
-    @Primary
-//primary annotation is used when a class is fetching data from 2 or more classes, which leads to priority issue
-// In devil.java , the build() calls a method compile() which is present in both laptop and desktop , @primary is used in these scenario to give precedence
-// the class required a single bean but 2 were found
-
-
-public class laptop implements computer {
+@Component
+public class desktop implements computer {
 
     public void compile(){
 
-        System.out.println("compiling with 404 bugs");
+        System.out.println("compiling with 402 bugs");
     }
 }
 //                   +----------------------------------------------------------------------+
